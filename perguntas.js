4330ccaaf143430a562376a1d7186965af2e19b4
Dourado019 <168429956+Dourado019@users.noreply.github.com>
2024-11-01 14:07:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'CORINTHIANS',
+    'Em qual competição o Corinthians conquistou o seu primeiro título internacional?',
+    'Na Copa do Mundo de Clubes da FIFA, em 2000'
+)
+
+criaCartao(
+    'CORINTHIANS',
+    'Quando foi fundado o Corinthians?',
+    'Em 1º de setembro de 1910'
+)
+
+criaCartao(
+    'CORINTHIANS',
+    'Quantas vezes o Corinthians ganhou o Campeonato Brasileiro?',
+    'O Corinthians conquistou o Campeonato Brasileiro 7 vezes (até 2023)'
+)
+
+criaCartao(
+    'CORINTHIANS',
+    'Em que ano o Corinthians venceu a Copa do Mundo de Clubes da FIFA pela primeira vez?',
+    'Em 2000, e novamente em 2012'
+)
